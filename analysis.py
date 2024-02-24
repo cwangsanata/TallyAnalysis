588d446de1f17a2c5e50b295299fdc042495ed58
Chris Wangsanata <chriswangsanata@gmail.com>
2024-02-23 16:36:22 -0800

    initial commit

---
+++ analysis.py
@@ -0,0 +1,25 @@
+def main():
+    with open('survey.txt', 'r') as file:
+        data = file.readlines()
+    data = [int(line.strip()) for line in data]
+    file.close()
+
+    print_statistics(data)
+
+def sample_mean(data):
+    return sum(data) / len(data)
+
+def sample_variance(data):
+    mean = sample_mean(data)
+    return sum((x - mean) ** 2 for x in data) / (len(data) - 1)
+
+def sample_standard_deviation(data):
+    return sample_variance(data) ** 0.5
+
+def print_statistics(data):
+    print(f'Sample Mean: {sample_mean(data)}')
+    print(f'Sample Variance: {sample_variance(data)}')
+    print(f'Sample Standard Deviation: {sample_standard_deviation(data)}')
+
+if __name__ == '__main__':
+    main()
